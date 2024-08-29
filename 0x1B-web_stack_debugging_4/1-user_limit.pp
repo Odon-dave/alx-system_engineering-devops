688b9e0ff6ba6ea1c22c865d6e7a1694042c5650
Odon-dave <odondave9@gmail.com>
2024-08-29 14:50:43 -0700

    0x1B-web_stack_debugging_4

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Enable the user holberton to log in and open files without errors
+
+# Increase the hard file limit for the holberton user
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase the soft file limit for the holberton user
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
